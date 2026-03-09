c1344021b330649f2ba404cf99f597b830a1d19b
muhammadmiqdartif25 <muhammad.miqdar.tif25@polban.ac.id>
2026-03-09 21:35:33 +0700

    First Commit: Init Project. Bismillah.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR     lpCmdLine,
+                   int       nCmdShow
+                   ) {
+    MessageBox(0, "Hello", "Archivista", 0);
+
+    return 0;
+}
