622f29a463eb1b09f2d5bbcaa42cba89c3d659b6
Wildan M Zaki <wildanmzaki7@gmail.com>
2026-03-10 11:06:55 +0700

    chore: use Archivista name for exe file

--- debug.bat
+++ debug.bat
@@ -1,4 +1,4 @@
 if exist build\debug rmdir /s /q build\debug
 cmake -S . -B build/debug -G "Ninja" -DCMAKE_BUILD_TYPE=Debug
 cmake --build build/debug -j
-.\build\debug\Proyek_2.exe
+.\build\debug\Archivista.exe
