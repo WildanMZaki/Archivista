f57a59456ef9912fc8ed6b71d596a4432ff78995
muhammadmiqdartif25 <muhammad.miqdar.tif25@polban.ac.id>
2026-04-10 14:34:09 +0700

    refactor: add SetSelection helper and integrate cursor updates

--- Source/selection.c
+++ Source/selection.c
@@ -1,24 +1,21 @@
 #include "../Header/selection.h"
-#include "../Header/main.h"
+#include "../Header/cursor.h"
+
+static void SetSelection(AppState *s, int startrow, int startcol, int endrow, int endcol) { // Helper function to make it more clean
+    s->selection.active = 1;
+    s->selection.start.row = startrow;
+    s->selection.start.col = startcol;
+    s->selection.end.row = endrow;
+    s->selection.end.col = endcol;
+}
 
 void Selection_SelectPoint(AppState *s, int row, int col) {
-    s->selection.active = 1;
-    s->selection.start.row = row;
-    s->selection.start.col = col;
-    s->selection.end.row = row;
-    s->selection.end.col = col;
-
-    s->textBuffer.cursorRow = row;
-    s->textBuffer.cursorCol = col;
+    SetSelection(s, row, col, row, col);
+    Cursor_SetPosition(&s->textBuffer, row, col);
 }
 void Selection_SelectLine(AppState *s, int row) {
-    s->selection.active = 1;
-    s->selection.start.row = row;
-    s->selection.start.col = 0;
-    s->selection.end.row = row;
-    s->selection.end.col = s->textBuffer.lineLen[row];
-    s->textBuffer.cursorRow = row;
-    s->textBuffer.cursorCol = s->textBuffer.lineLen[row];
+    SetSelection(s, row, 0, row, s->textBuffer.lineLen[row]);
+    Cursor_SetPosition(&s->textBuffer, row, s->textBuffer.lineLen[row]);
 }
 void Selection_SelectWord(AppState *s, int row, int col) {
     int startCol = col;
@@ -30,21 +27,11 @@ void Selection_SelectWord(AppState *s, int row, int col) {
     while (endCol < len && isalnum((unsigned char)s->textBuffer.lines[row][endCol])) {
         endCol++;
     }
-    s->selection.active = 1;
-    s->selection.start.row = row;
-    s->selection.start.col = startCol;
-    s->selection.end.row = row;
-    s->selection.end.col = endCol;
-    s->textBuffer.cursorRow = row;
-    s->textBuffer.cursorCol = endCol;
+    SetSelection(s, row, startCol, row, endCol);
+    Cursor_SetPosition(&s->textBuffer, row, endCol);
 }
 
 void Selection_SelectAll(AppState *s) {
-    s->selection.active = 1;
-    s->selection.start.row = 0;
-    s->selection.start.col = 0;
-    s->selection.end.row = s->textBuffer.lineCount - 1;
-    s->selection.end.col = s->textBuffer.lineLen[s->selection.end.row];
-    s->textBuffer.cursorRow = s->selection.end.row;
-    s->textBuffer.cursorCol = s->selection.end.col;
+    SetSelection(s, 0, 0, s->textBuffer.lineCount - 1, s->textBuffer.lineLen[s->selection.end.row]);
+    Cursor_SetPosition(&s->textBuffer, s->selection.end.row, s->selection.end.col);
 }
