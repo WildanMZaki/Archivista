e1f5433f0002a94b757feaf058b53558f19b082a
Wildan M Zaki <wildanmzaki7@gmail.com>
2026-04-10 13:10:14 +0700

    fix: specify single job for cmake build in debug.bat

--- debug.bat
+++ debug.bat
@@ -1,4 +1,4 @@
 if exist build\debug rmdir /s /q build\debug
 cmake -S . -B build/debug -G "Ninja" -DCMAKE_BUILD_TYPE=Debug
-cmake --build build/debug -j
+cmake --build build/debug -j1
 .\build\debug\Archivista.exe
