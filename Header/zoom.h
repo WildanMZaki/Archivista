6d3b6dde08b479203ba515be1d08498c25edd9c1
muhammadmiqdartif25 <muhammad.miqdar.tif25@polban.ac.id>
2026-05-28 16:07:34 +0700

    feat: zoom in/out feature

---
+++ Header/zoom.h
@@ -0,0 +1,8 @@
+//
+// Created by thori on 28/05/2026.
+//
+
+#ifndef ARCHIVISTA_ZOOM_H
+#define ARCHIVISTA_ZOOM_H
+
+#endif //ARCHIVISTA_ZOOM_H
---
+++ Source/zoom.c
@@ -0,0 +1,5 @@
+//
+// Created by thori on 28/05/2026.
+//
+
+#include "zoom.h"
